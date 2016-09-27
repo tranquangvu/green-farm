c283e21be19ff920370eebeb75212d0509002685
Tranxuanvu <xuanvu442@gmail.com>
2016-09-27 23:34:31 +0700

    Layout (WIP: header, section offer)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,28 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require owl.carousel
+
+$(window).load(function() {
+  $('#loader').delay(3000).fadeOut('slow');
+  $('#loader-container').delay(2000).fadeOut('slow');
+  $('body').delay(3000).css({'overflow':'visible'});
+})
+
+$('#owl-sticky').owlCarousel({
+  loop:true,
+  margin:30,
+  nav:true,
+  dots:false,
+  responsive:{
+    0:{
+      items:1
+    },
+    600:{
+      items:2
+    },
+    1000:{
+      items:3
+    }
+  }
+})
---
+++ app/models/sensor.rb
@@ -0,0 +1,6 @@
+class Sensor
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  
+end
