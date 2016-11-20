f1d16b205a1054f10071ddf1b7745e3e99be6eaa
tranquangvu <vutran.513@gmail.com>
2016-11-20 16:40:02 +0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,34 @@ user = User.create(
 )
 
 farm = user.farms.create(
-  name: 'My first farm',
-  address: '666/64/5 Ly Thuong Kiet, TPHCM'
+  name: 'SGardern',
+  address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
 )
 
-farm.create_device(
+device = farm.create_device(
   ip: '192.168.0.114'
 )
 
-farm.create_camera(CameraCreating.build(
+camera = farm.create_camera(CameraCreating.build(
   ip: 'http://je4452.myfoscam.org',
   port: '133',
   username: 'guest',
   password: 'vutran_513'
 ))
+
+start_time = Time.parse('2016-11-01 00:00:00')
+4320.times do |time|
+  temperature   = rand(20...40)
+  humidity      = rand(0..100)
+  soil_moisture = rand(0..100)
+  light         = rand(2000...3000)
+
+  Value.create(
+    light: light,
+    humidity: humidity,
+    temperature: temperature,
+    soil_moisture: soil_moisture,
+    created_at: start_time + time*10.minutes,
+    device: device,
+  )
+end
