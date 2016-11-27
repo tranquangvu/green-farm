9ef7fbbfb31d12038b41f8f821fb34a0d2827ca0
tranquangvu <vutran.513@gmail.com>
2016-11-27 11:42:46 +0700

    add method to get avg of value

--- app/models/value.rb
+++ app/models/value.rb
@@ -20,4 +20,98 @@ class Value
   def time
     created_at
   end
+
+  def self.avg_in_hour_of_month(year:, month:, prop:)
+    $project = {
+      "$project" => {
+        "year" => {
+          "$year" => "$created_at"
+        },
+        "month" => {
+          "$month" => "$created_at"
+        },
+        "hour" => {
+          "$hour" => "$created_at"
+        },
+        "#{prop}" => "$#{prop}",
+      }
+    }
+
+    $match = {
+      "$match" => {
+        "year" => year,
+        "month" => month
+      }
+    }
+
+    $group = {
+      "$group" => {
+        "_id" => {
+          "year" => "$year",
+          "month" => "$month",
+          "hour" => "$hour"
+        },
+        "avg_#{prop}" => {
+          "$avg" => "$#{prop}"
+        }
+      }
+    }
+
+    $sort = {
+      "$sort" => {
+        "_id" => 1
+      }
+    }
+
+    self.collection.aggregate([$project, $match, $group, $sort])
+  end
+
+  def self.avg_in_minute_of_month(year:, month:, prop:)
+    $project = {
+      "$project" => {
+        "year" => {
+          "$year" => "$created_at"
+        },
+        "month" => {
+          "$month" => "$created_at"
+        },
+        "hour" => {
+          "$hour" => "$created_at"
+        },
+        "minute" => {
+          "$minute" => "$created_at"
+        },
+        "#{prop}" => "$#{prop}",
+      }
+    }
+
+    $match = {
+      "$match" => {
+        "year" => year,
+        "month" => month
+      }
+    }
+
+    $group = {
+      "$group" => {
+        "_id" => {
+          "year" => "$year",
+          "month" => "$month",
+          "hour" => "$hour",
+          "minute" => "$minute",
+        },
+        "avg_#{prop}" => {
+          "$avg" => "$#{prop}"
+        }
+      }
+    }
+
+    $sort = {
+      "$sort" => {
+        "_id" => 1
+      }
+    }
+
+    self.collection.aggregate([$project, $match, $group, $sort])
+  end
 end
