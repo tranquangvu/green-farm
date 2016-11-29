9e9a64c9af934cc8bcc01e9ef52e74d17ff6c18f
tranquangvu <vutran.513@gmail.com>
2016-11-29 23:36:28 +0700

    load data to report

--- app/controllers/inspecter/farms_controller.rb
+++ app/controllers/inspecter/farms_controller.rb
@@ -9,6 +9,14 @@ class Inspecter::FarmsController < Inspecter::BaseController
   end
 
   def report
+    @start_date = report_params[:start_time] || current_date
+    @end_date = report_params[:end_date] || current_date
+    @props = report_params[:props].map(&:to_sym) || all_query_props
+
+    @data = @farm.values.where(
+      :created_at.gte => "#{@start_date} 00:00:00",
+      :created_at.lte => "#{@end_date} 23:59:59"
+    ).only(*@props, :created_at)
   end
 
   def chart
@@ -18,5 +26,17 @@ class Inspecter::FarmsController < Inspecter::BaseController
 
   def set_farm
     @farm = Farm.find(params[:id])
+  end
+
+  def all_query_props
+    [:temperature, :humidity, :light, :soil_moisture]
+  end
+
+  def current_date
+    Time.now.strftime('%Y-%m-%d')
+  end
+
+  def report_params
+    params.permit(:start_date, :end_date, props: [])
   end
 end
